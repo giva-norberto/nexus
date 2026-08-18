bd7eb555a4878928a151c3216a6be569b8f9edec
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2026-08-17 21:31:19 -0300

    feat: exportar Nexus Agent Core

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -2,10 +2,12 @@ const core = require('./index');
 const conversationMemory = require('./conversation-memory');
 const firebaseObserver = require('./firebase-observer');
 const firestoreExplorer = require('./firestore-explorer');
+const agentCore = require('./agent-core');
 
 module.exports = {
   ...core,
   ...conversationMemory,
+  ...agentCore,
   firebaseProjectStatus: firebaseObserver.firebaseProjectStatus,
   firebaseFirestoreRead: firestoreExplorer.firebaseFirestoreRead,
   firebaseSpendingAnalytics: firestoreExplorer.firebaseSpendingAnalytics
