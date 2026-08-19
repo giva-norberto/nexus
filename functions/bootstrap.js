948128a0075cd05c13120627b752550b2f5e7e46
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2026-08-18 21:40:45 -0300

    Ativar Agent Core v1.5 com fallback Gemini

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -3,13 +3,13 @@ const conversationMemory = require('./conversation-memory');
 const firebaseObserver = require('./firebase-observer');
 const firestoreExplorer = require('./firestore-explorer');
 const agentCore = require('./agent-core');
-const agentCoreV14 = require('./agent-core-v14');
+const agentCoreV15 = require('./agent-core-v15');
 
 module.exports = {
   ...core,
   ...conversationMemory,
   ...agentCore,
-  askNexusAgent: agentCoreV14.askNexusAgent,
+  askNexusAgent: agentCoreV15.askNexusAgent,
   firebaseProjectStatus: firebaseObserver.firebaseProjectStatus,
   firebaseFirestoreRead: firestoreExplorer.firebaseFirestoreRead,
   firebaseSpendingAnalytics: firestoreExplorer.firebaseSpendingAnalytics
