7c3e272bdb9088b72148e4f1aa19c728fdaf2ca3
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2026-08-19 21:31:33 -0300

    chore: ativar Agent Core v2.2

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -3,13 +3,13 @@ const conversationMemory = require('./conversation-memory');
 const firebaseObserver = require('./firebase-observer');
 const firestoreExplorer = require('./firestore-explorer');
 const agentCore = require('./agent-core');
-const agentCoreV21 = require('./agent-core-v21');
+const agentCoreV22 = require('./agent-core-v22');
 
 module.exports = {
   ...core,
   ...conversationMemory,
   ...agentCore,
-  askNexusAgent: agentCoreV21.askNexusAgent,
+  askNexusAgent: agentCoreV22.askNexusAgent,
   firebaseProjectStatus: firebaseObserver.firebaseProjectStatus,
   firebaseFirestoreRead: firestoreExplorer.firebaseFirestoreRead,
   firebaseSpendingAnalytics: firestoreExplorer.firebaseSpendingAnalytics
